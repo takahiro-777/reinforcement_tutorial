77ff04f360da69a7fecda29451f12aa85eba9f48
takahiro <sequence2014@gmail.com>
2017-10-05 17:49:38 +0900

    grasp how_to_use_CartPole-v0 #12

--- gym/CartPole/2.py
+++ gym/CartPole/2.py
@@ -4,6 +4,7 @@ for i_episode in range(20):
     observation = env.reset() # reset for each new trial
     for t in range(100): # run for 100 timesteps or until done, whichever is first
         env.render()
+        #print(observation)
         action = env.action_space.sample() # select a random action (see https://github.com/openai/gym/wiki/CartPole-v0)
         observation, reward, done, info = env.step(action)
         if done:
--- gym/CartPole/3.py
+++ gym/CartPole/3.py
@@ -3,8 +3,11 @@ env = gym.make('CartPole-v0')
 highscore = 0
 for i_episode in range(20): # run 20 episodes
     observation = env.reset()
+    step = 0
     points = 0 # keep track of the reward each episode
     while True: # run until episode is done
+    #for t in range(100):
+        step +=1
         env.render()
         action = 1 if observation[2] > 0 else 0 # if angle if positive, move right. if angle is negative, move left
         observation, reward, done, info = env.step(action)
@@ -12,4 +15,5 @@ for i_episode in range(20): # run 20 episodes
         if done:
             if points > highscore: # record high score
                 highscore = points
-                break
+            print("Episode finished after {} timesteps".format(step+1))
+            break
